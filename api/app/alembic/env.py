9a4fda80c1c1290ee56547c079fda4dbdb0bf875
Weidong Sun <sunweidong90@gmail.com>
2021-03-06 12:03:48 -0600

    [backend]: fix alembic env

--- api/app/alembic/env.py
+++ api/app/alembic/env.py
@@ -1,9 +1,11 @@
+import sys
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+sys.path.insert(0, ".")
+from app.core.config import SQLALCHEMY_DATABASE_URI
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +19,11 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.db.session import Base
+
+# from app.db.models.account import Base as AccountBase
+# from app.db.models.transaction import Base as TransactionBase
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -25,19 +31,21 @@ target_metadata = None
 # ... etc.
 
 
+def get_url():
+    return SQLALCHEMY_DATABASE_URI
+
+
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
-    url = config.get_main_option("sqlalchemy.url")
+    # url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -51,21 +59,19 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
@@ -75,3 +81,4 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
+
--- api/app/core/config.py
+++ api/app/core/config.py
@@ -2,6 +2,6 @@ import os
 
 PROJECT_NAME = "nomorewait"
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://admin@localhost/nomorewait")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres@localhost/nomorewait")
 
 API_V1_STR = "/api/v1"
