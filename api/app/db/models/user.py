c2c43d0f0df7faa352d3651e1e4f36f448e6b1b9
Weidong Sun <sunweidong90@gmail.com>
2021-03-06 13:08:38 -0600

    [backend]: fix user email typo

--- api/app/db/models/user.py
+++ api/app/db/models/user.py
@@ -12,7 +12,7 @@ class User(Base):
     # columns
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
-    emails = Column(String)
+    email = Column(String)
     password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
