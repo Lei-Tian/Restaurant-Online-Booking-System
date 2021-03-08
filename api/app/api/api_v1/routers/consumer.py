77ffd98986b1cc1d4eac4ecb46b17344c8990322
XiaoxianMa <sunweidong90@gmail.com>
2021-03-08 13:04:50 -0600

    [BACKEND] create consume APIs

---
+++ api/app/api/api_v1/routers/consumer.py
@@ -0,0 +1,53 @@
+import typing as t
+
+from fastapi import APIRouter, Depends, Request, Response
+
+from app.core.auth import get_current_active_user
+from app.db.session import get_db
+from app.db.schemas.consumer import (
+  SearchIn, SearchedAvailableRestaurant,
+  SelectTableIn, SelectTableSuccessOut, SelectTableFailOut,
+  OrderIn, OrderSuccessOut, OrderFailOut,
+)
+
+consumer_router = r = APIRouter()
+
+
+@r.post("/search", response_model=t.List[SearchedAvailableRestaurant])
+async def user_create(
+    request: Request,
+    search_in: SearchIn,
+    db=Depends(get_db),
+    current_user=Depends(get_current_active_user),
+):
+    """
+    Find all available restaurants and their available windows
+    """
+    return []
+
+  
+@r.post("/select-table", response_model=t.Union[SelectTableSuccessOut, SelectTableFailOut])
+async def user_create(
+    request: Request,
+    select_table_in: SelectTableIn,
+    db=Depends(get_db),
+    current_user=Depends(get_current_active_user),
+):
+    """
+    Select a table
+    """
+    return
+  
+  
+ @r.post("/order", response_model=t.Union[OrderSuccessOut, OrderFailOut])
+async def user_create(
+    request: Request,
+    select_table_in: OrderIn,
+    db=Depends(get_db),
+    current_user=Depends(get_current_active_user),
+):
+    """
+    Confirm an order
+    """
+    return
+   
