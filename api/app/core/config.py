2f0652beaaa8b0f669b9ed248bb7924f585d4593
happyprincewu <44780324+happyprincewu@users.noreply.github.com>
2021-04-03 00:00:19 -0400

    Update config.py

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -2,7 +2,7 @@ import os
 
 PROJECT_NAME = "nomorewait"
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres@localhost/nomorewait_ziyan")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres@localhost/nomorewait")
 REDIS_URI = os.getenv("REDIS_URL", "redis://localhost")
 
 API_V1_STR = "/api/v1"
